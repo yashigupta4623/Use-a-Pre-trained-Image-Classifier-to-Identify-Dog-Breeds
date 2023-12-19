b7d6e6bc3350281304dcc1e185e3e85dc83d0bae
Yashi Gupta <98335217+yashigupta4623@users.noreply.github.com>
2023-12-19 16:47:23 +0530

    Update calculates_results_stats.py

--- calculates_results_stats.py
+++ calculates_results_stats.py
@@ -4,7 +4,7 @@
 #                                                                             
 # PROGRAMMER: Yashi Gupta
 # DATE CREATED: 15/12/2023                                 
-# REVISED DATE: 
+# REVISED DATE: 19/12/2023
 # PURPOSE: Create a function calculates_results_stats that calculates the 
 #          statistics of the results of the programrun using the classifier's model 
 #          architecture to classify the images. This function will use the 
