a428cbf60bbe9ac7c52725febc2dd55c5e521ff4
Yashi Gupta <98335217+yashigupta4623@users.noreply.github.com>
2023-12-19 16:47:06 +0530

    Update calculates_results_stats.py

--- calculates_results_stats.py
+++ calculates_results_stats.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/calculates_results_stats.py
 #                                                                             
-# PROGRAMMER: Shamil Jamion
-# DATE CREATED: 19/04/2019                                 
+# PROGRAMMER: Yashi Gupta
+# DATE CREATED: 15/12/2023                                 
 # REVISED DATE: 
 # PURPOSE: Create a function calculates_results_stats that calculates the 
 #          statistics of the results of the programrun using the classifier's model 
